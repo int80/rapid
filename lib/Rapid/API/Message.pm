f32f07c05d9c0a97288924e6ae4d9168547cd64f
Mischa S <revmischa@cpan.org>
2012-06-14 12:34:37 -0700

    keep camera_id when replying to a rapid message

--- lib/Rapid/API/Message.pm
+++ lib/Rapid/API/Message.pm
@@ -20,6 +20,18 @@ has 'transport' => (
 sub reply {
     my ($self, $reply_msg) = @_;
 
+    # keep some params if we're replying
+    my $orig_params = $self->params;
+    my $reply_params = $reply_msg->params;
+
+    # params to keep
+    my @keep = qw/camera_id/;
+
+    foreach my $p (@keep) {
+        $reply_params = $orig_params->{$p}
+            if exists $orig_params->{$p} && ! exists $reply_params->{$p};
+    }
+
     $self->transport->push_message($reply_msg);
 }
 
