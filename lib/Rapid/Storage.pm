29f42657b977a72f397ccd0eb2dd38ad2ee7e848
Mischa S <revmischa@cpan.org>
2012-06-16 22:11:00 -0700

    use MXSD for dbic serilization

--- lib/Rapid/Storage.pm
+++ lib/Rapid/Storage.pm
@@ -2,20 +2,9 @@ package Rapid::Storage;
 
 use Moose::Role;
 use namespace::autoclean;
-use Rapid::Storage::Basic;
+with 'MooseX::Storage::DBIC';
 
-# make some fields serializable
-sub serializable {
-    my ($class, @fields) = @_;
-
-    # need to define some attributes for the storage engine to pick up
-#     foreach my $f (@fields) {
-#         $class->meta->add_attribute($f => ( is => 'bare' ));
-#     }
-
-    # init our version of MooseX::Storage
-    my @storage_roles = qw/Rapid::Storage::Basic/;
-    $_->meta->apply($class->meta) for @storage_roles;
-}
+use Rapid qw/$schema/;
+sub schema { $schema }
 
 1;
